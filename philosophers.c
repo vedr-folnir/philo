0c6d15da28f73d89ffd7ffe4a0ddabf1e5f18e79
vedr-folnir <124672580+vedr-folnir@users.noreply.github.com>
2023-11-02 10:21:36 +0100

    avant wipe

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: hlasota <hlasota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 10:23:09 by hlasota           #+#    #+#             */
-/*   Updated: 2023/10/20 13:10:48 by hlasota          ###   ########.fr       */
+/*   Updated: 2023/11/02 10:19:01 by hlasota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philo.h"
@@ -63,13 +63,68 @@ int	error(t_val *val)
 
 void	ft_try_print(char *text, t_philo *philo, long int time, int id)
 {
-	if (philo->vargs->death != 1)
+
+	if (philo->vargs->death != -1)
 	{
-		printf("%ld %d %s\n", time, id, text);
-		if (philo->vargs->death == -1)
-			philo->vargs->death = 1;
+		printf("%ld %d %s\n", actual_time() - time, id, text);
 	}
 	return;
+}
+
+/*int	ft_eat(t_philo *philo)
+{
+	if (pthread_mutex_lock(&philo->vargs->l_fork[philo->id - 1]) == 0
+		&& pthread_mutex_lock(&philo->vargs->l_fork[philo->id
+		% (philo->vargs->nb_philo)]) == 0)
+	{
+		ft_try_print("has taken a fork", philo, philo->vargs->start, philo->id);
+		ft_try_print("has taken a fork", philo, philo->vargs->start, philo->id);
+		ft_try_print("is eating", philo, philo->vargs->start, philo->id);
+		ft_usleep(philo->vargs->time_eat);
+		philo->meals_eat++;
+		philo->last_eat = actual_time();
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id - 1]);
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id
+			% philo->vargs->nb_philo]);
+		if (philo->vargs->death == -1)
+			return (-1);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id - 1]);
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id
+			% philo->vargs->nb_philo]);
+		return (-1);
+	}
+	return (0);
+}*/
+
+int	ft_eat(t_philo *philo)
+{
+	while (42)
+	{	
+		if (philo->last_eat < philo->vargs->time_sleep + 5)
+			continue;
+		pthread_mutex_lock(&philo->vargs->l_fork[philo->id - 1]);
+		ft_try_print("has taken a fork", philo, philo->vargs->start, philo->id);
+		pthread_mutex_lock(&philo->vargs->l_fork[philo->id
+			% (philo->vargs->nb_philo)]);
+		ft_try_print("has taken a fork", philo, philo->vargs->start, philo->id);
+		ft_try_print("is eating", philo, philo->vargs->start, philo->id);
+		ft_usleep(philo->vargs->time_eat);
+		philo->meals_eat++;
+		printf("%d  %ld\n", philo->id, actual_time() - philo->last_eat);
+		philo->last_eat = actual_time();
+		printf("%d  %ld\n", philo->id, actual_time() - philo->last_eat);
+		ft_try_print("finish eating", philo, philo->vargs->start, philo->id);
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id - 1]);
+		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id
+			% philo->vargs->nb_philo]);
+		if (philo->vargs->death == -1)
+			return (-1);
+		break;
+	}
+	return (0);
 }
 
 void *routine(void *data)
@@ -79,60 +134,20 @@ void *routine(void *data)
 	philo = (t_philo *)data;
 	philo->meals_eat = 0;
 	if (philo->id % 2 == 0)
-		usleep(philo->vargs->time_eat * 1000);
+		usleep(philo->vargs->time_eat * 100);
+	printf("%ld philo %d vient de se reveiller\n", actual_time() - philo->vargs->start,  philo->id);
 	while (42 && philo->meals_eat != philo->vargs->nb_meal)
 	{	
+		//printf("debut routine %d\n", philo->id);
 		if (philo->vargs->death == -1)
-		{
-			
-			ft_try_print("has died", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-			//printf("%ld %d has died\n", (long int)(actual_time() - philo->vargs->start), philo->id);
-			return (0);
-		}
-		pthread_mutex_lock(&philo->vargs->l_fork[philo->id - 1]);
-		
-
-		if (philo->vargs->death == -1)
-		{
-			
-			ft_try_print("has died", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-			//printf("%ld %d has died\n", (long int)(actual_time() - philo->vargs->start), philo->id);
-			return (0);
-		}
-		
-		ft_try_print("has taken a fork", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-		//printf("%ld %d has taken a fork %d\n", (long int)(actual_time() - philo->vargs->start), philo->id, philo->id - 1);
-		pthread_mutex_lock(&philo->vargs->l_fork[philo->id % (philo->vargs->nb_philo)]);
-		
-		if (philo->vargs->death == -1)
-		{
-			
-			ft_try_print("has died", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-			//printf("%ld %d has died\n", (long int)(actual_time() - philo->vargs->start), philo->id);
-			return (0);
-		}
-
-		ft_try_print("has taken a fork", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-		//printf("%ld %d has taken a fork %d\n", (long int)(actual_time() - philo->vargs->start), philo->id, philo->id % (philo->vargs->nb_philo));
-		ft_try_print("is eating", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-		//printf("%ld %d is eating\n", (long int)(actual_time() - philo->vargs->start), philo->id);
-		ft_usleep(philo->vargs->time_eat);
-		philo->meals_eat++;
-		philo->last_eat = actual_time();
-		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id - 1]);
-		pthread_mutex_unlock(&philo->vargs->l_fork[philo->id % philo->vargs->nb_philo]);
-		ft_try_print("is sleeping", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-		//printf("%ld %d is sleeping\n", (long int)(actual_time() - philo->vargs->start), philo->id);
+			break;
+		if (ft_eat(philo) == -1)
+			continue;
+		ft_try_print("is sleeping", philo, philo->vargs->start, philo->id);
 		ft_usleep(philo->vargs->time_sleep);
-
-		if (philo->vargs->death == -1)
-		{
-			
-			ft_try_print("has died", philo, (long int)(actual_time() - philo->vargs->start), philo->id);
-			//printf("%ld %d has died\n", (long int)(actual_time() - philo->vargs->start), philo->id);
-			return (0);
-		}
-		
+		if (philo->meals_eat == philo->vargs->nb_meal)
+			break;
+		ft_try_print("is thinking", philo, philo->vargs->start, philo->id);
 	}
 	return (0);
 }
@@ -170,7 +185,6 @@ void joinThreads(t_val *threadData)
     while (i < threadData->nb_philo)
     {
         pthread_join(threadData->l_philo[i].thread_id, NULL);
-        //pthread_mutex_destroy(&threadData->l_fork[i]); // Destruction de la fourchette du philo
         i++;
     }
 }
@@ -185,6 +199,7 @@ void	ft_death(t_val *val)
 		if (actual_time() - val->l_philo[i].last_eat >= val->time_die)
 		{
 			val->death = -1;
+			printf("%ld %d has died\n", actual_time() - val->start, i + 1);
 			return;
 		}
 		if (val->l_philo[i].meals_eat == val->nb_meal)
@@ -197,6 +212,8 @@ int	main(int argc, char **argv)
 {
 	t_val	val;
 	int		check_error;
+	int		i;
+
 	if (parse_args(argc, argv, &val) != 1)
 	{
 		printf("wrong args\n");
@@ -208,5 +225,16 @@ int	main(int argc, char **argv)
 	createThreads(&val);
 	ft_death(&val);
 	joinThreads(&val);
+	i = 0;
+	/*while (i < val.nb_philo)
+	{
+		if (pthread_mutex_destroy(&val.l_fork[i]) != 0) {
+            perror("Erreur lors de la destruction du mutex");
+            // Gérer l'erreur ici, si nécessaire
+        } else {
+            i++;
+        }
+	}*/
+
 	return (0);
 }
